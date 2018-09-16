d420d9370f5a62b73c619d96f99da26ef0b428af
ivshyam98 <ivshyam1998@gmail.com>
2018-09-16 16:34:53 +0530

    volunteer details posted to firebase

--- events/src/pages/signup/signup.ts
+++ events/src/pages/signup/signup.ts
@@ -4,6 +4,7 @@ import {NgForm} from '@angular/forms';
 import {AuthService} from '../../services/auth';
 import { EventlistPage } from '../eventlist/eventlist';
 import { Http, Response } from "@angular/http";
+import firebase from 'firebase';
 import { map } from 'rxjs/operators';
 import { auth } from '../../../node_modules/firebase';
 /**
@@ -19,8 +20,17 @@ import { auth } from '../../../node_modules/firebase';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+  public name : string;
+  public email : string;
+  public phone : number; 
+  public gender : any;
+  public profession : any;
+  public userProfile: any;
+ 
+  
   @ViewChild('password') password;
   constructor(private http:Http, private alertCtrl:AlertController,private loadCtrl: LoadingController, private authService: AuthService, public navCtrl: NavController, public navParams: NavParams) {
+    this.userProfile = firebase.database().ref('volunteers');
   }
 
   ionViewDidLoad() {
@@ -28,18 +38,29 @@ export class SignupPage {
   }
   
   onSignUp(form:NgForm){
+    var volunteerdetails = {
+      name: form.value.name,
+      email: form.value.email,
+      phone: form.value.phone,
+      gender: form.value.gender,
+      profession: form.value.profession
+    };
     let loading = this.loadCtrl.create({
       content: 'Creating user...'
     });
   
     loading.present();
-    console.log(this.password.value);
+   
     console.log(form.value.email);
     this.authService.signUp(form.value.email,this.password.value)
     .then(data=>{
 
       console.log(data);
       loading.dismiss();
+      console.log(this.authService.getActiveUser().uid);
+      this.userProfile.child(this.authService.getActiveUser().uid).set(
+        volunteerdetails
+      );
       this.navCtrl.push(EventlistPage);
       
       })

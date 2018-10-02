f4c04491b6398836d45be0668417e6f9ac0aa32d
ivshyam98 <ivshyam1998@gmail.com>
2018-10-02 23:13:08 +0530

    Added new firebase credentials

--- events/src/app/app.component.ts
+++ events/src/app/app.component.ts
@@ -21,7 +21,12 @@ export class MyApp {
 
   constructor(private authService:AuthService,platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     firebase.initializeApp({
-     "Your firebase credentials"
+      apiKey: "AIzaSyD_8CkpNmdpzQacmLD2vDmB8IrFCHkefrc",
+     authDomain: "volunteercall-a2de1.firebaseapp.com",
+     databaseURL: "https://volunteercall-a2de1.firebaseio.com",
+     projectId: "volunteercall-a2de1",
+     storageBucket: "volunteercall-a2de1.appspot.com",
+     messagingSenderId: "333057397510"
     });
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
@@ -44,4 +49,3 @@ export class MyApp {
     this.authService.logOut();
   }
 }
-
--- events/src/app/firebase.credentials.ts
+++ events/src/app/firebase.credentials.ts
@@ -1,3 +1,8 @@
 export const firebase_credentials = {
-  "Your firebase credentials"
+  apiKey: "AIzaSyD_8CkpNmdpzQacmLD2vDmB8IrFCHkefrc",
+   authDomain: "volunteercall-a2de1.firebaseapp.com",
+   databaseURL: "https://volunteercall-a2de1.firebaseio.com",
+   projectId: "volunteercall-a2de1",
+   storageBucket: "volunteercall-a2de1.appspot.com",
+   messagingSenderId: "333057397510"
 }

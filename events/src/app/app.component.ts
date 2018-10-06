63448d64726e2b4aca085eb3ce6595df9db981b3
Mohit Kumar Toshniwal <mohitkrtoshniwal@gmail.com>
2018-10-06 06:40:48 -0700

    Update app.component.ts

--- events/src/app/app.component.ts
+++ events/src/app/app.component.ts
@@ -26,12 +26,7 @@ export class MyApp {
   pages: Array<{title: string, component: any}>;
   constructor(private authService:AuthService,platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     firebase.initializeApp({
-      apiKey: "AIzaSyDGfFA_nMECpByJdoydqyHfEuDwRftftOo",
-      authDomain: "volunteerapp-b1364.firebaseapp.com",
-      databaseURL: "https://volunteerapp-b1364.firebaseio.com",
-      projectId: "volunteerapp-b1364",
-      storageBucket: "volunteerapp-b1364.appspot.com",
-      messagingSenderId: "547161890493"
+      "Your credentials"
     });
     firebase.auth().onAuthStateChanged(user => {
       if (user) {

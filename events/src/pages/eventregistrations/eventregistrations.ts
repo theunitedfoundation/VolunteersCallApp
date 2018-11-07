304da9f23b5cf2cbabf69804459d417c3126562a
mohitkumartoshniwal <mohitkrtoshniwal.com>
2018-11-07 16:45:02 +0530

    Added phone and email intent

--- events/src/pages/eventregistrations/eventregistrations.ts
+++ events/src/pages/eventregistrations/eventregistrations.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,AlertController } from 'ionic-angular';
 import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
 import {AuthService} from '../../services/auth'
 import { Observable } from 'rxjs';
+import { CallNumber } from '@ionic-native/call-number';
+import { EmailComposer } from '@ionic-native/email-composer';
 /**
  * Generated class for the EventregistrationsPage page.
  *
@@ -31,7 +33,8 @@ export class EventregistrationsPage {
 
   }
   eventregistrationsListRef$ : Observable<any[]>
-  constructor(private database: AngularFireDatabase,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private database: AngularFireDatabase,public navCtrl: NavController, public navParams: NavParams,
+    private callNumber: CallNumber, alertCtrl: AlertController,private emailComposer: EmailComposer) {
     this.eventregistrationsListRef$ = this.database.list('eventlist/'+this.eventItem.key+'/registrations').valueChanges();
   }
 
@@ -40,4 +43,26 @@ export class EventregistrationsPage {
     console.log(this.navParams.get('key'));
   }
 
+  Call(phone){
+    this.callNumber.callNumber(phone, true)
+    .then(res => console.log('Launched dialer!', res))
+    .catch(err => console.log('Error launching dialer', err));
+   }
+   Email(emailaddress){
+    this.emailComposer.isAvailable().then((available: boolean) =>{
+      if(available) {
+        //Now we know we can send
+      }
+     });
+     
+     let email = {
+       to: emailaddress,
+       subject: this.eventItem.eventName,
+       isHtml: true
+     };
+     
+     // Send a text message using default options
+     this.emailComposer.open(email);
+   }
+  
 }
